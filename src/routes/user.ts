a67f443c4840b798a6790caa46c1ea3a1ec6b5e3
lzy671 <lzy.671@outlook.com>
2024-05-14 20:06:39 +0800

    feat:update user api

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
-import { getData } from '@/utils';
+import { base64Decode, base64Encode, getData, setData } from '@/utils';
 import Router from 'koa-router'
 
 const userRoutes = new Router();
 
 interface UserInfo {
+  id?: number;
   phone: string;
   password: string;
   avatar?: string;
@@ -22,20 +23,34 @@ userRoutes.post('/api/user/login', ctx => {
   const reqData = ctx.request.body as LoginRequestParams;
   const users = getData('users') as (UserInfo[] | undefined);
   if (Array.isArray(users)) {
-    const result = users.find(item => item.phone == reqData.phone);
+    const result = users.find(item => item.phone == reqData.phone && item.password == reqData.password);
     if (result) {
-      const { password, ...info } = result;
+      const loginCookie = {
+        id: result.id,
+        times: new Date().getTime()
+      }
+      const token = base64Encode(JSON.stringify(loginCookie))
+      ctx.cookies.set('token', token);
       ctx.body = {
         code: 0,
         message: 'success!',
-        data: info as LoginResponseParams
+        data: {
+          ...result,
+          password: undefined,
+        } as LoginResponseParams
       };
       return
     }
+    ctx.body = {
+      code: 0,
+      message: 'Check whether the parameters are correct!',
+      data: null
+    };
+    return
   }
   ctx.body = {
     code: -1,
-    message: 'The user is not exist!',
+    message: 'The account is not exist!',
     data: null
   };
 })
@@ -46,10 +61,86 @@ interface RegisterRequestParams {
 }
 
 userRoutes.post('/api/user/regist', ctx => {
-  ctx.body = "hello regist"
+  const data = ctx.request.body as RegisterRequestParams;
+  if (!data.password || !data.phone) {
+    ctx.body = {
+      code: -1,
+      message: 'The paramter is missing!',
+      data: null
+    };
+    return
+  }
+  const users = getData('users') as (UserInfo[] | undefined);
+  const target = {
+    decription: '',
+    avatar: '',
+    nickname: '',
+    ...data,
+    id: new Date().getTime()
+  } as UserInfo;
+  if (Array.isArray(users)) {
+    const result = users.find(item => item.phone == data.phone);
+    if (result) {
+      ctx.body = {
+        code: -1,
+        message: 'The account is exist!',
+        data: null
+      };
+      return
+    }
+    users.push(target)
+    setData(users, 'users');
+    ctx.body = {
+      code: 0,
+      message: 'success!',
+      data: null
+    };
+    return
+  }
+  setData([target], 'users')
+  ctx.body = {
+    code: 0,
+    message: 'success!',
+    data: null
+  };
 })
 
-
-
+type UserEditRequestParams = Omit<UserInfo, 'id'>;
+userRoutes.post('/api/user/edit', ctx => {
+  const data = ctx.request.body as UserEditRequestParams;
+  const users = getData('users') as (UserInfo[] | undefined);
+  if (Array.isArray(users)) {
+    const sign = JSON.parse(base64Decode(ctx.cookies.get('token') || ''))
+    if (!sign?.id) {
+      ctx.body = {
+        code: -1,
+        message: 'sign is missing',
+        data: null
+      }
+      return
+    }
+    const result = users.map(item => {
+      if(item.id == sign.id){
+        return {
+          ...item,
+          ...data
+        }
+      }
+      return item
+    });
+    setData(result, 'users');
+    ctx.body = {
+      code: 0,
+      message: 'success!',
+      data: null
+    };
+    return
+  }
+  ctx.body = {
+    code: -1,
+    message: 'Illegal user!',
+    data: null
+  }
+})
 
 export default userRoutes;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -21,15 +21,14 @@ const cache_file = path.resolve(dirName, 'cache/data.json');
 })()
 
 // 将数据写入JSON文件
-export const setData = (data: Partial<any>, propertyPath?: string) => {
+export const setData = (data: Partial<any>, propertyPath: string) => {
+  if (!fs.existsSync(cache_file)) {
+    fs.writeFileSync(cache_file, JSON.stringify({}, null, 2));
+  }
   const dataJSON = fs.readFileSync(cache_file, 'utf8');
   const value = JSON.parse(dataJSON)
-  if (propertyPath) {
-    const result = set(value, propertyPath, data);
-    fs.writeFileSync(cache_file, JSON.stringify(result, null, 2));
-    return
-  }
-  fs.writeFileSync(cache_file, JSON.stringify(data, null, 2));
+  const result = set(value, propertyPath, data);
+  fs.writeFileSync(cache_file, JSON.stringify(result, null, 2));
 };
 
 // 从JSON文件读取数据
@@ -45,13 +44,19 @@ export const getData = (propertyPath?: string) => {
 };
 
 // 保存上传的文件
-export const saveFile = (file:any) => {
+export const saveFile = (file: any) => {
   const fileHash = crypto.createHash('md5').update(file).digest('hex');
   const filePath = path.join(dirName, 'assets', fileHash);
   fs.writeFileSync(filePath, file);
   return fileHash;
 };
 
-export const getFile=(fileHash:string)=>{
+export const base64Encode = (str: string) => {
+  const buffer = Buffer.from(str, 'utf-8');
+  return buffer.toString('base64');
+};
 
-}
+export const base64Decode = (str: string) => {
+  const buffer = Buffer.from(str, 'base64');
+  return buffer.toString('utf-8');
+};
